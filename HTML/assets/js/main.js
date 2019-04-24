05dad6ad5effa39c788ea3233d65c003ed2c7039
Akash Javali <akashjavali@gmail.com>
2019-04-24 12:20:15 +0530

    init

---
+++ HTML/assets/js/main.js
@@ -0,0 +1,8 @@
+/* Loader */
+
+$(window).on('load', function () {
+    setTimeout(function () {
+        $('.loader').fadeOut("slow");
+    }, 1000);
+});
+/* Loader Ends */
---
+++ PHP/footer.php
@@ -0,0 +1,25 @@
+<footer class="">
+    <div class="container">
+        <!-- <p>© 2019 <b>Project Name</b>. All Rights Reserved.</p> -->
+    </div>
+</footer>
+
+
+
+<!-- scrips -->
+
+<!-- jQuery -->
+<script src="lib/jquery-3.3.1.min.js"></script>
+
+<!-- popper.min -->
+<script src="lib/popper.min.js"></script>
+
+<!-- Plugins -->
+<script src="lib/bootstrap.min.js"></script>
+
+<script src="lib/swiper.min.js"></script>
+
+<script src="lib/owl.carousel.min.js"></script>
+
+<!-- custom scripts -->
+<script src="assets/js/main.js"></script>
