7769a163e202b799aaa6433106e3205a6e331ecc
akashjavali <akash.j@pacewisdom.com>
2022-05-20 23:30:33 +0530

    added few changes to html

---
+++ HTML/assets/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    // Wait for window load
+    // $(window).on('load', function () {
+    //     // Animate loader off screen
+    //     $('.se-pre-con').fadeOut('slow');
+    // });
+
+    /* navbar onscroll color change functionality */
+    // $(document).scroll(function () {
+    //     if ($(this).scrollTop() > 100) {
+    //         $('.fixed-top').addClass('change_navColor', 'main_nav');
+    //     } else {
+    //         $('.fixed-top').removeClass('change_navColor', 'main_nav');
+    //     }
+    // });
+
+    /* Scroll to top js code */
+    // document.body.scrollTop = document.documentElement.scrollTop = 0;
+    // $(document).scroll(function () {
+    //     if ($(this).scrollTop() > 100) {
+    //         $('.scroll_top_icon').css('bottom', '20px');
+    //     } else {
+    //         $('.scroll_top_icon').css('bottom', '-50px');
+    //     }
+    // });
+
+    // $('.scroll_top_icon').click(function () {
+    //     $('html, body').animate(
+    //         {
+    //             scrollTop: 0,
+    //         },
+    //         1400
+    //     );
+    // });
+
+    // var swiper = new Swiper('.my_swiper', {
+    //     spaceBetween: 30,
+    //     centeredSlides: true,
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 10000,
+    //         disableOnInteraction: false,
+    //     },
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    //     },
+    // });
+});
